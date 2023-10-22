7e8a5b0b4bfdb2058b3b9232b7206013b1a069ba
anti1hero1 <nikitadanikita@gmail.com>
2023-10-22 19:27:17 +0300

    deploy1

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
